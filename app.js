bad1ac4f6daf34cbd315038c13d494186ebbe7a9
jaleyerdelen <jaleyerdelen@gmail.com>
2021-11-18 00:56:01 +0300

    fixed again

--- app.js
+++ app.js
@@ -3,23 +3,22 @@ const express = require("express");
 const app = express();
 
 //Template engine
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 //Middlewares
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 //Routes
 app.get("/", (req, res) => {
   res.status(200).render("index", {
-    page_name: "index"
+    page_name: "index",
   });
 });
 app.get("/about", (req, res) => {
   res.status(200).render("about", {
-    page_name: "about"
+    page_name: "about",
   });
 });
-
 
 const port = 3000;
 app.listen(port, () => {
