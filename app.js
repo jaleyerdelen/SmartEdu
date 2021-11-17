083ba9e17c223119c92cca852db1b0e7c41cbabe
jaleyerdelen <jaleyerdelen@gmail.com>
2021-11-18 01:24:13 +0300

    mvc routing created

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const pageRoute = require("./routes/pageRoute")
 const app = express();
 
 //Template engine
@@ -9,16 +9,8 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 //Routes
-app.get("/", (req, res) => {
-  res.status(200).render("index", {
-    page_name: "index",
-  });
-});
-app.get("/about", (req, res) => {
-  res.status(200).render("about", {
-    page_name: "about",
-  });
-});
+app.use("/", pageRoute);
+
 
 const port = 3000;
 app.listen(port, () => {
