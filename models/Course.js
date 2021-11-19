30cc52f6e933878b51491e2ec1cc597a31d8c1ad
jaleyerdelen <jaleyerdelen@gmail.com>
2021-11-19 15:28:44 +0300

    slugify used

--- models/Course.js
+++ models/Course.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const slugify = require("slugify");
 
 const CourseSchema = new Schema({
   name: {
@@ -16,7 +17,19 @@ const CourseSchema = new Schema({
     type: Date,
     default: Date.now,
   },
+  slug: {
+    type: "string",
+    unique: true,
+  }
 });
+
+CourseSchema.pre('validate', function(next){
+  this.slug = slugify(this.name, {
+    lower: true,
+    strict: true,
+  })
+  next()
+})
 
 const Course = mongoose.model("Course", CourseSchema);
 module.exports = Course;
