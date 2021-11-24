4e8025735936e815002cb4d39c923cb4889f3db8
jaleyerdelen <jaleyerdelen@gmail.com>
2021-11-24 12:58:15 +0300

    installed session

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const pageRoute = require("./routes/pageRoute")
 const mongoose = require("mongoose");
+const session = require("express-session");
 const courseRoute = require("./routes/courseRoute")
 const categoryRoute = require("./routes/categoryRoute");
 const userRoute = require("./routes/userRoute");
@@ -23,12 +24,27 @@ mongoose
 //Template engine
 app.set("view engine", "ejs");
 
+//global variables
+global.userIN = null;
+
+
 //Middlewares
+
 app.use(express.static("public"));
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
+app.use(
+  session({
+    secret: "my_keyboard_cat",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 //Routes
+app.use("*", (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
 app.use("/categories", categoryRoute);
--- controllers/authController.js
+++ controllers/authController.js
@@ -27,7 +27,8 @@ exports.loginUser =  (req, res) => {
       bcrypt.compare(password, user.password, (err, same)=> {
         if(same){
           //user session
-          res.status(200).send("You are logged in")
+          req.session.userID = user._id;
+          res.status(200).redirect("/")
         }
       })
     }
