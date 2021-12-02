452b415a88edcdbbe604e307876d1f370af2957c
jaleyerdelen <jaleyerdelen@gmail.com>
2021-12-02 15:06:22 +0300

    dashboard page

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,7 +28,7 @@ exports.loginUser =  (req, res) => {
         if(same){
           //user session
           req.session.userID = user._id;
-          res.status(200).redirect("/")
+          res.status(200).redirect("/users/dashboard")
         }
       })
     }
@@ -47,6 +47,12 @@ exports.logoutUser = (req, res) => {
   })
 }
 
-    
+ exports.getDashboardPage = async (req, res) => {
+   const user = await User.findOne({_id: req.session.userID})
+   res.status(200).render("dashboard", {
+     page_name: "dashboard",
+     user
+   });
+ };   
   
 
